9cb4e02c4b005969dabf713548f9cb41ecfd5431
Thomas <warbeaver@pm.me>
2019-12-14 23:04:20 +0100

    much, much cleaner coding (previous was complete garbage)

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const path = require('path')
 // app represents the logic of the server.
 const app = express()
 // http is the server
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
 
 // mark 'public' as the public dir
 app.use(express.static(path.join(__dirname, 'public')))
@@ -12,71 +12,72 @@ app.use(express.static(path.join(__dirname, 'public')))
 // all routing logic is handled by routes/router.js
 app.use('', require('./routes/router'))
 
-/* ALL rooms MUST have different ids */
-let room1 = {
-	id: 1,
-	'population': 0
+/* array of available chat rooms */
+/* a socket should be part of less than one 
+* of these at any given time */
+let chat_rooms = ['1', '2']
+let nb_chat_rooms = chat_rooms.length
+
+function searchForChatRoom(socket, room_index) {
+    /* Recursively search for an available chat room.
+    * If chat room of index 'room_index' in array 'chat_rooms' 
+    * full or not responding: try next, if existing. */
+    let room = chat_rooms[room_index]
+    console.log('trying room: ' + room)
+    io.in(room).clients( (error, clients) => {
+        console.log('clients for room ' + room + ': ' + clients)
+        if (error) throw error
+        if (clients.length < 2) {
+            socket.join(room, () => {
+                console.log('socket ' + socket.id + ' joined room ' + room)
+            })
+        } else if (room_index + 1 < nb_chat_rooms) {
+            searchForChatRoom(socket, room_index + 1)
+        } else {    
+            console.log('no available room found for socket ' + socket.id + ', searching again in 1s')
+            setTimeout(searchForChatRoom, 1000, socket, 0)
+        }
+    })
 }
 
-let room2 = {
-	id: 2,
-	'population': 0
-}
+/* ///entry point\\\ */
+io.on('connect', (socket) => {
+    console.log('socket ' + socket.id + ' just connected')
 
-let waitingroom = {
-	id: 0,
-	'population': 0
-}
+    // all users join general
+    socket.join('general', () => {
+        console.log('socket ' + socket.id + ' joined general')
+    })
+    
+    // check for an available chat room
+    searchForChatRoom(socket, 0)
 
-// array of available rooms
-let rooms = [room1, room2]
-
-function changeRoom(socket, room) {
-	/* Make socket leave its previous room (if existing), and join 'room'. 
-	* Asummption: socket is only part of one room (beside its own). */
-	if (socket.room != undefined) { // in case of first connection
-		socket.leave(socket.room.id) // leave previous room
-		socket.room.population -= 1
-	}
-	socket.join(room.id) // join new room
-	socket.room = room.id // set or change .room attribute
-	room.population +=1
-	// tell the user to connect on new room
-	// sending the whole room json object
-	socket.emit('room change', room)
-	console.log('User "' + socket.id + '" was told to connect on room ' + socket.room)
-}
-
-// first connection = "entry point"
-io.on('connection', (socket) => {
-	// waitingroom is default at connection
-	changeRoom(socket, waitingroom)
-
-	// check for an available room, do what is necessary
-	for (i=0; i<rooms.length; i++) {
-		if (rooms[i].population < 2) {
-			changeRoom(socket, rooms[i])
-			break
-		}
-	}
-
-	// wait for a chat message
+    // wait for a chat message
 	socket.on('chat message', (msg) => {
-		console.log('Received chat message "' + msg + '" from room ' + socket.room +'. Sending it back')
-		//send the message to the room
-		io.to(socket.room).emit('chat message', msg)
-	})
+        //find the room it originates from
+        rooms = Object.keys(socket.rooms)
+        let emitting_room
+        for (room of rooms) {
+            if (room != socket.id && room != 'general') {
+                emitting_room = room
+                break
+            }
+        }
+        console.log('Received chat message "' + msg + '" from room ' + emitting_room +'. Sending it back')
+        if (emitting_room == undefined) {
+            console.log("error: couldn't find emitting room!")
+        } else {
+            //send the message to the room
+            io.to(emitting_room).emit('chat message', msg)
+            //socket.broadcast.to(room_id).emit('chat message', msg)
+        }
+        
 
-	// disconnection handling
-	socket.on('disconnect', () => {
-		// call of socket.leave() on all its channels is automatic
-		socket.room.population -= 1
-		console.log('User "' + socket.id + '" disconnected from room ' + socket.room)
 	})
 })
 
 http.listen(8080, () => {
-	console.log('Listening on port 8080')
+	console.log('----- Listening on port 8080 -----')
 })
 
 module.exports = app
