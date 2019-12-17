1d50bc7d2d6ed0ab11eb067be357feb8b84013e6
Julien Lin <julien.lin@ensta-paris.fr>
2019-12-17 17:16:59 +0100

    fck

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require('express')
+const path = require('path')
 
 // app represents the server.
 const app = express()
 
+app.use(express.static(path.join(__dirname__, 'public')))
+
 app.get('/', function(req, res) {
-	res.send('Salut')
+    res.send('Salut')
 })
 
 app.use('/route', require('./routes/route'))
 app.listen(8080, () => {
-	console.log('Listening on port 8080')
+    console.log('Listening on port 8080')
 })
 
 module.exports = app
