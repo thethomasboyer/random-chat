61b937e879d88e73d05d1d825d2e23d2a3e24153
Thomas <warbeaver@pm.me>
2019-12-17 20:28:37 +0100

    pt Julien t'as reverse tous mes commits

--- public/js/init.js
+++ public/js/init.js
@@ -1,52 +1,113 @@
+
 var socket = io()
+
 /* callback on id='sendButton' button on click event */
-function listenToSendButton() {
-    // get the message input element
-    let inputField = document.getElementById("userMessageInputField")
-    // get its value
-    let message = inputField.value
-    // do nothing if message is empty
-    if (message == "") {
-        return false
-    }
-    // otherwise send it to the room...
-    socket.emit('chat message', message)
-    // ...and empty the input field
-    inputField.value = ""
-    console.log("Message sent: '" + message + "' to room: " + socket.room)
+function sendMessage() {
+	// get the message input element
+	let inputField = document.getElementById('userMessageInputField')
+	// get its value
+	let message = inputField.value
+	// do nothing if message is empty
+	if (message == '') {
+		return false
+	}
+	// otherwise send it to the room...
+	// ... with the saved room number, for simplicity
+	socket.emit('chat message', message)
+	// ...and empty the input field
+	inputField.value = ''
+	appendNewMessage(message)
+}
+
+/* enter key triggers SendButton */
+function enterKeySendsMessage() {
+	document.addEventListener('keydown', (e) => {
+		if (e.keyCode === 13) {// "Enter" key code
+			sendMessage()
+		}
+	})
+}
+
+/* Check if user is typing */
+function userIsTyping() {
+	var searchTimeout
+	document.getElementById('userMessageInputField').onkeypress = function () {
+		if (searchTimeout != undefined) clearTimeout(searchTimeout)
+		searchTimeout = setTimeout(function () {
+			socket.emit('user typing')
+		}, 250)
+	}
+}
+
+/* update the page to display new message */
+function appendNewMessage(msg) {
+	// get already displayed messages
+	let messages = document.getElementById('messages')
+	// display new ones
+	let new_message = document.createElement('li')
+	new_message.appendChild(document.createTextNode(msg))
+	messages.appendChild(new_message)
+	window.scrollTo(0, document.body.scrollHeight)
 }
 
 /* handling newly received messages */
 function listenToIncomingMessages() {
-    socket.on('chat message', (msg) => {
-        console.log("Message received: " + msg + " on room: " + socket.room)
-        // get already displayed messages
-        let messages = document.getElementById("messages")
-        // display new ones
-        let new_message = document.createElement('li')
-        new_message.appendChild(document.createTextNode(msg));
-        messages.appendChild(new_message)
-    })
+	socket.on('chat message', (msg) => {
+		appendNewMessage(msg)
+	})
+
+	socket.on('user typing', () => {
+		console.log('User is typing')
+	})
+
+	socket.on('change interloc', () => {
+		document.getElementById('messages').innerHTML = ''
+		console('Now talking to a brand new face')
+	})
 }
 
-/* handling room changes */
-function listenToRoomChange() {
-    // if a 'room change' message is received...
-    socket.on('room change', (room_id) => {
-        /* ...clean messages here...
-        * but useles because no messages are supposed to
-        * have been sent. No ? :) */
-        // change the socket room
-        // beware that socket.room here is socket.room.id on server
-        socket.room = room_id
-        console.log("Received room change order, now on room " + socket.room)
-    })
+/* print the people count */
+function printPeopleCount() {
+	socket.on('greeting', (data) => {
+		let notif = '<font size="2"><span uk-icon=\'icon: user\'></span> ' + data.newcommer + ' vient de se connecter !</font>'
+		UIkit.notification({ message: notif, pos: 'top-right' })
+		const count = data.peoplecount + 1
+		let people_counter = document.getElementById('peopleCounter')
+		if (count > 1) {
+			people_counter.innerHTML = 'Il y a actuellement ' + count + ' personnes connectées !'
+		} else {
+			people_counter.innerHTML = 'Vous êtes seul(e)... :\'('
+		}
+	})
+
+	socket.on('byebye', (data) => {
+		console.log(data.leaver + ' just left the Random Chat!')
+		const count = data.peoplecount--
+		let people_counter = document.getElementById('peopleCounter')
+		if (count > 1) {
+			people_counter.innerHTML = 'Il y a actuellement ' + count + ' personnes connectées !'
+		} else {
+			people_counter.innerHTML = 'Vous êtes seul(e)... :\'('
+		}
+	})
+}
+
+/* change interlocutor */
+function changeInterlocutor() {
+	socket.emit('change interloc')
+	console.log('asked to change interlocutor, must be boring')
 }
 
 /* global logic entry point */
 function init() {
-    // start listening to room change
-    listenToRoomChange()
-    // start listening to incom..
-    listenToIncomingMessages()
+	// start listening to incom..
+	listenToIncomingMessages()
+	// print the people count
+	printPeopleCount()
+	// respond to "Enter" key press
+	enterKeySendsMessage()
+	// blabla
+	userIsTyping()
 }
+
+init() // client-side logic starts here
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+/* eslint-disable indent */
+
 const express = require('express')
 const path = require('path')
 // app represents the logic of the server.
@@ -18,41 +20,101 @@ app.use('', require('./routes/router'))
 let chat_rooms = ['1', '2']
 let nb_chat_rooms = chat_rooms.length
 
-function searchForChatRoom(socket, room_index) {
-    /* Recursively search for an available chat room.
-    * If chat room of index 'room_index' in array 'chat_rooms' 
-    * full or not responding: try next, if existing. */
-    let room = chat_rooms[room_index]
-    console.log('trying room: ' + room)
-    io.in(room).clients((error, clients) => {
-        console.log('clients for room ' + room + ': ' + clients)
-        if (error) throw error
-        if (clients.length < 2) {
+function changeRoom(socket, room, first_connection = false) {
+    /* Make socket leave its current chat room (if existing),
+    * and join 'room' */
+    //get user's current chat room
+    let socket_room = findRoom(socket, first_connection)
+    //if found...
+    if (socket_room != undefined) {
+        //...leave it...
+        socket.leave(socket_room, () => {
+            console.log('socket ' + socket.id + ' left room ' + room)
+            //...and join new one AFTER that
             socket.join(room, () => {
                 console.log('socket ' + socket.id + ' joined room ' + room)
             })
+        })
+    } else socket.join(room, () => {
+        console.log('socket ' + socket.id + ' joined room ' + room)
+    })
+}
+
+function searchForChatRoom(socket, room_index = 0, first_connection = false) {
+    /* Recursively search for an available chat room.
+    * If chat room of index 'room_index' in array 'chat_rooms' 
+    * full, try next, if existing. */
+    let room = chat_rooms[room_index]
+    io.in(room).clients((error, clients) => {
+        if (error) console.log(error)
+        if (clients.length < 2) {
+            changeRoom(socket, room, first_connection)
         } else if (room_index + 1 < nb_chat_rooms) {
-            searchForChatRoom(socket, room_index + 1)
+            searchForChatRoom(socket, room_index + 1, first_connection)
         } else {
-            console.log('no available room found for socket ' + socket.id + ', searching again in 1s')
-            setTimeout(searchForChatRoom, 1000, socket, 0)
+            console.log('no available room found for socket ' + socket.id + ', searching again in 3s')
+            setTimeout(searchForChatRoom, 3000, socket)
         }
     })
 }
 
-function findRoom(socket) {
-    rooms = Object.keys(socket.rooms)
+function searchForChatRoomAvoidUser(socket, user, room_index = 0) {
+    /* Recursively search for an available chat room, avoiding 'user'.
+    * If chat room of index 'room_index' in array 'chat_rooms' 
+    * full, or 'user' in it, try next, if existing. */
+    let room = chat_rooms[room_index]
+    io.in(room).clients((error, clients) => {
+        if (error) console.log(error)
+        //try less than 1 member in room and 'user' not member
+        if (clients.length < 2 && !(user in clients)) {
+            changeRoom(socket, room)
+        } else if (room_index + 1 < nb_chat_rooms) {
+            //if not suitable, try next chat room, if existing
+            searchForChatRoomAvoidUser(socket, user, room_index + 1)
+        } else { //else retry whole search process later
+            console.log('no room found for socket ' + socket.id + ', searching again in 3s')
+            setTimeout(searchForChatRoom, 3000, socket)
+        }
+    })
+}
+
+
+function findRoom(socket, first_connection = false) {
+	/* Return the emitting room the socket is in.
+    * Assumes that the socket is only part of 'general', 
+    * one chat room or less, and its own. 
+    * Will log to console if not found, unless 
+    * first connection specified  */
+    let rooms = Object.keys(socket.rooms)
     let emitting_room
-    for (room of rooms) {
+    for (let room of rooms) {
         if (room != socket.id && room != 'general') {
             emitting_room = room
             break
-        }
+        } // .filter ?
     }
-    if (emitting_room == undefined) {
-        console.log("error: couldn't find emitting room!")
-        return false
+    if (emitting_room == undefined && !first_connection) {
+        console.log('error: couldn\'t find emitting room!')
     } else return emitting_room
+}
+
+function getInterloc(socket) {
+	/* Get the interlocutor of a socket.
+    * Assumes there are no more than 2 people by chat room. */
+    let emitting_room = findRoom(socket)
+    if (emitting_room != undefined) {
+        let interloc
+        io.in(emitting_room).clients((error, clients) => {
+            if (error) console.log(error)
+            for (let client of clients) {
+                if (client != socket.id) {
+                    interloc = client
+                    break
+                }
+            }
+        })
+        return interloc
+    } else console.log('error: couldn\'t find interlocutor!')
 }
 
 /* ///entry point\\\ */
@@ -71,14 +133,14 @@ io.on('connect', (socket) => {
     })
 
     /* check for an available chat room, loop until found */
-    searchForChatRoom(socket, 0)
+    searchForChatRoom(socket, 0, true)
 
     /* transmit chat messages to adequate rooms */
     socket.on('chat message', (msg) => {
         //find the room it originates from
         let emitting_room = findRoom(socket)
-        console.log('Received chat message "' + msg + '" from room ' + emitting_room + ', transmitting')
-        if (emitting_room != false) {
+        if (emitting_room != undefined) {
+            console.log('Received chat message "' + msg + '" from room ' + emitting_room + ', transmitting')
             //send the message back to the room, but not to the sender
             socket.broadcast.to(emitting_room).emit('chat message', msg)
         }
@@ -88,16 +150,28 @@ io.on('connect', (socket) => {
     socket.on('user typing', () => {
         //find the room it originates from
         let emitting_room = findRoom(socket)
-        console.log('Received user typing message from room ' + emitting_room + ', transmitting')
-        if (emitting_room != false) {
+        if (emitting_room != undefined) {
+            console.log('Received user typing message from room ' + emitting_room + ', transmitting')
             //send the message back to the room, but not to the sender
             socket.broadcast.to(emitting_room).emit('user typing')
         }
     })
 
+    /* change interlocutor */
+    socket.on('change interloc', () => {
+        console.log('User ' + socket.id + ' asked to change interlocutor')
+        let interloc = getInterloc(socket)
+        searchForChatRoomAvoidUser(socket, interloc)
+    })
+
     /* handle disconnection */
     socket.on('disconnect', (reason) => {
         console.log('socket ' + socket.id + ' just left ; cause: ' + reason)
+        io.in('general').clients((error, clients) => {
+            if (error) console.log(error)
+            io.to('general').emit('byebye', { leaver: socket.id, peoplecount: clients.length }
+            )
+        })
     })
 })
 
