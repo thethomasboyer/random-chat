4db52b3f05f648d569e52f00d351809a9727ded5
Thomas <warbeaver@pm.me>
2020-02-07 00:51:57 +0100

    nothing

--- routes/router.js
+++ routes/router.js
@@ -3,11 +3,11 @@ const path = require('path')
 const router = express.Router()
 
 router.get('/', (_, res) => {
-	res.sendFile(path.join(__dirname, '../views', 'index.html'))
+    res.sendFile(path.join(__dirname, '../views', 'index.html'))
 })
 
-router.use( (_, res) => {
-	res.status(404).sendFile(path.join(__dirname, '../views', '404.html'))
+router.use((_, res) => {
+    res.status(404).sendFile(path.join(__dirname, '../views', '404.html'))
 })
 
 module.exports = router
